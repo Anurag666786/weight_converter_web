e532285039d8e1560674af5412a06e2624c108f4
Anurag <itsanurag666@gmail.com>
2025-04-06 20:54:52 +0530

    updated index.html and app.py

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ def index():
             "kg": weight * 1000,
             "mg": weight * 0.001,
             "q": weight * 100_000,
-            "lb": weight * 453.59237
+            "lb": weight * 453.59237,
+            "g": weight * 1,
+            "oz": weight * 28.3495231,
+            "ct": weight * 0.2,
+            "gr": weight * 0.06479891,
+            "st": weight * 6350.29318
         }
 
         weight_in_grams = conversions.get(original_unit, 0)
@@ -28,7 +33,12 @@ def index():
             "kg": weight_in_grams / 1000,
             "mg": weight_in_grams / 0.001,
             "q": weight_in_grams / 100_000,
-            "lb": weight_in_grams / 453.59237
+            "lb": weight_in_grams / 453.59237,
+            "g": weight_in_grams / 1,
+            "oz": weight_in_grams / 28.3495231,
+            "ct": weight_in_grams / 0.2,
+            "gr": weight_in_grams / 0.06479891,
+            "st": weight_in_grams / 6350.29318
         }
 
         result = round(to_unit.get(new_unit, 0), 4)
